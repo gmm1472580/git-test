169aa3f65a4ee659cbb4c20b848b76154178413c
gmm <18437959958@163.com>
2019-02-18 15:09:54 +0800

    上传git

---
+++ git/git/src/com/itheima/git/TestClass.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+    }
+    public void test(){
+        //测试git
+        int i=0;
+        i+=10;
+        System.out.println(i);
+    }
+
+}
